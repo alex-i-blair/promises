a5128db8d1dbb436323f161bde3c59ac0e4a7722
Alex Blair <bclan11@gmail.com>
2022-04-27 21:55:34 -0700

    finished all async and .then exercises

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
-import { asyncGetQuotes, thenGetQuotes } from './services/promise-me';
+import {
+  asyncGetQuotes,
+  thenGetQuotes,
+  asyncTryGetQuotes,
+  catchDotThenQuotes,
+  asyncGetQuotesFinally,
+  getQuotesDotFinally,
+  exerciseSeven,
+  exerciseEight,
+} from './services/promise-me';
 
 export default function App() {
-  const handleClick = (callback) => {
-    const res = callback();
-    console.log(res);
-  };
   return (
     <>
-      <button onClick={() => handleClick(asyncGetQuotes)}>
+      <button onClick={async () => console.log(await asyncGetQuotes())}>
         Async Get Quotes
       </button>
-      <button onClick={() => handleClick(thenGetQuotes)}>
+      <button onClick={async () => console.log(await thenGetQuotes())}>
         .then() Get Quotes
+      </button>
+      <button onClick={async () => console.log(await asyncTryGetQuotes())}>
+        Async Try get
+      </button>
+      <button onClick={async () => console.log(await catchDotThenQuotes())}>
+        catch .then quotes
+      </button>
+      <button onClick={async () => console.log(await asyncGetQuotesFinally())}>
+        Async catch finally
+      </button>
+      <button onClick={async () => console.log(await getQuotesDotFinally())}>
+        DotFinally
+      </button>
+      <button onClick={async () => console.log(await exerciseSeven())}>
+        Exercise 7
+      </button>
+      <button onClick={async () => console.log(await exerciseEight())}>
+        Exercise 8
       </button>
     </>
   );
--- src/services/promise-me.js
+++ src/services/promise-me.js
@@ -18,23 +18,48 @@ export function thenGetQuotes() {
 /**
  * TODO: Exercise 3: use `fetch`, async/await, and a try/catch statement to get data from an API and handle errors
  */
-
+export async function asyncTryGetQuotes() {
+  try {
+    return await asyncGetQuotes();
+  } catch (error) {
+    console.error('error');
+  }
+}
 /**
  * TODO: Exercise 4: use `fetch`, `.then`, and `.catch` to get the same data from exercise 3 while handling errors
  */
-
+export function catchDotThenQuotes() {
+  return asyncGetQuotes().catch((error) => console.error('error'));
+}
 /**
  * TODO: Exercise 5: use `fetch`, async/await, and a try/catch/finally statement to get data from an API, handle errors, then console.log('All done!') upon completion
  */
-
+export async function asyncGetQuotesFinally() {
+  try {
+    return await asyncGetQuotes();
+  } catch (error) {
+    console.error('error');
+  } finally {
+    console.log('All done!');
+  }
+}
 /**
  * TODO: Exercise 6: use `fetch`, `.then`, `.catch`. and `.finally` to get the same data from exercise 5 while handling errors and calling console.log('All done!') upon completion
  */
-
+export function getQuotesDotFinally() {
+  return thenGetQuotes()
+    .catch((error) => console.error('error'))
+    .finally(() => console.log('All done!'));
+}
 /**
- * TODO: Exercise 7: Call your function from exercise 7 using .then
+ * TODO: Exercise 7: Call your function from exercise 5 using .then
  */
-
+export function exerciseSeven() {
+  return asyncGetQuotesFinally().then((res) => console.log(res));
+}
 /**
  * TODO: Exercise 8: Call your function from exercise 6 using async/await
  */
+export async function exerciseEight() {
+  return await getQuotesDotFinally();
+}
